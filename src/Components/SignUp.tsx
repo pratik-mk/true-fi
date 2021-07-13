761b318ee7233b87b2af64bb0193f34838a3394f
Pratik Dhoke <pratikdhoke13@gmail.com>
2021-07-13 15:08:28 +0530

    sign-up validation added

--- src/Components/SignUp.tsx
+++ src/Components/SignUp.tsx
@@ -1,46 +1,151 @@
-import React from 'react';
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
+import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-interface Props {
-
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        TrueFi
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export const SignUp: React.FC<Props> = (props) => {
-    return (
-        <>
-            <div>
-                <AppBar position="static">
-                    <Toolbar>
-                        <IconButton
-                            edge="start"
-                            color="inherit"
-                            aria-label="open drawer"
-                        >
-                        </IconButton>
-                        <Typography variant="h6" noWrap>
-                            True-Fi
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-            </div>
-            <h1>Sign Up</h1>
-            <div>
-                <TextField type="email" label="Email" variant="outlined" />
-            </div>
-            <div>
-                <TextField label="Password" variant="outlined" />
-            </div>
-            <div>
-                <TextField label="Confirm Password" variant="outlined" />
-            </div><div>
-                <Button variant="contained" color="primary">Submit</Button>
-            </div>
-        </>
-    );
-};
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const [repassword,setRepassword] = useState('');
+  const [errors, setErrors] = useState(false);
+
+  const handleSubmit = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    if(password !== repassword)
+    {
+        console.log('Password didnt match')
+        setErrors(true);
+        console.log(errors)
+    }
+    console.log('email: ',email)
+    console.log('password: ',password)
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign Up
+        </Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <TextField
+            value = {email}
+            type= 'email'
+            onChange = {(e) => setEmail(e.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            // {...(errors && {error:true, helperText: 'cannot be blank'})}
+          />
+          <TextField
+            value = {password}
+            onChange= {(e) => setPassword(e.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            {...(errors && {error:true, helperText: 'Password didnt match'})}
+          />
+          <TextField
+            value = {repassword}
+            onChange= {(e) => setRepassword(e.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Confirm Password"
+            type="password"
+            autoComplete="current-password"
+            {...(errors && {error:true, helperText: 'Password didnt match'})}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Already have an account? Sign in"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
