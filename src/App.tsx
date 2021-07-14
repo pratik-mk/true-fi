c01e8f0ecee704e4c9178605f2e35010bb50c7a8
Pratik Dhoke <pratikdhoke13@gmail.com>
2021-07-14 15:03:31 +0530

    Header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
 import Login from "./Components/Login"
 
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Login />
     </div>
   );
--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -3,10 +3,7 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
@@ -108,10 +105,6 @@ export default function Login() {
             autoComplete="current-password"
             {...(errors && {error:true, helperText: 'cannot be blank'})}
           />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
           <Button
             type="submit"
             fullWidth
@@ -121,21 +114,12 @@ export default function Login() {
           >
             Sign In
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
+          <Link href="#" variant="body2">
+            {"Don't have an account? Sign Up"}
+          </Link>
         </form>
       </div>
-      <Box mt={8}>
+      <Box mt={3}>
         <Copyright />
       </Box>
     </Container>
